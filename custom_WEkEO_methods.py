e9160a91b194687fadf9af7d027fb99a2c183199
Capizzi Emanuele <55940723+capizziemanuele@users.noreply.github.com>
2022-07-08 17:11:23 +0200

    Add files via upload

---
+++ custom_WEkEO_methods.py
@@ -0,0 +1,55 @@
+import xarray as xr
+import rioxarray as rxr
+from urllib.request import urlopen
+from ipyleaflet import Map, basemaps, basemap_to_tiles, DrawControl, LayersControl  #if error run: jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-leaflet
+
+
+def download_type(download_sel, download_list, get_url):
+    """
+    Function that can read the NetCDF file in memory or downloading it using its original filename.
+    """
+    url = get_url['content'][0]['url']
+    save_as = get_url['content'][0]['filename']
+    
+    if download_sel.value == "Read NETCDF in memory": 
+        fl = url
+        # load into memory 
+        with urlopen(fl) as f:
+            ds = xr.open_dataset(f.read())
+    elif download_sel.value == "Download NETCDF":
+        with urlopen(url) as file:
+            content = file.read()
+            with open(save_as, 'wb') as download:
+                download.write(content)
+            ds = xr.open_dataset(str(save_as))
+    return ds
+
+def draw_map(center_lat, center_lon, zoom_level):
+    """
+    Function to draw a map and interact with it. It is possible to get the coordinates values from the dc variable. Two basemaps are available.
+    """
+    satellite = basemap_to_tiles(basemaps.Gaode.Satellite)
+    osm = basemap_to_tiles(basemaps.OpenStreetMap.Mapnik)
+
+    cams_map = Map(layers=(satellite, osm ), center=(center_lat, center_lon), zoom=zoom_level)
+
+    dc = DrawControl()
+    lc = LayersControl(position='topright')
+
+    dc = DrawControl(
+        marker={"shapeOptions": {"color": "#0000FF"}},
+        rectangle={"shapeOptions": {"color": "#0000FF"}},
+        circle={"shapeOptions": {"color": "#0000FF"}},
+        circlemarker={},
+    )
+
+    def handle_draw(target, action, geo_json):
+        print(action)
+        print(geo_json)
+
+
+    dc.on_draw(handle_draw)
+    cams_map.add_control(dc)
+    cams_map.add_control(lc)
+    
+    return cams_map, dc
